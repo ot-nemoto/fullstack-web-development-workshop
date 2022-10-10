8c57bd3506d0944f851b00e4ccc72a4f0c89f542
n-itou <itou@opentone.co.jp>
2022-10-10 15:18:09 +0900

    model

--- backend/common/models.py
+++ backend/common/models.py
@@ -12,21 +12,23 @@ class Product(models.Model):
     """
     商品
     """
-    name = models.DateField(max_length=100, verbose_name='商品名')
+    name = models.CharField(max_length=100, verbose_name='商品名')
 
     class Meta:
         db_table = 'product'
         verbose_name = '商品'
 
 
-class Sales(models.Model):
+class Price(models.Model):
     """
-    売上
+    価格
     """
     price = models.IntegerField(verbose_name='販売価格')
+    start_date = models.DateField(verbose_name='開始日')
+    end_date = models.DateField(verbose_name='終了日')
     product = models.ForeignKey(
-        Product, on_delete=models.PROTECT, verbose_name='CPコード')
+        Product, on_delete=models.PROTECT, verbose_name='商品')
 
     class Meta:
-        db_table = 'sales'
-        verbose_name = '売上'
+        db_table = 'price'
+        verbose_name = '価格'
