84b5a946956ea4670406fb815121e8a4234bfbb6
n-itou <itou@opentone.co.jp>
2022-10-22 16:56:42 +0900

    独自メッセージ

--- backend/api/validation/serializers.py
+++ backend/api/validation/serializers.py
@@ -5,3 +5,8 @@ class ValidationSerializer(serializers.Serializer):
     delivery_date = serializers.DateField(help_text="納品日", required=True)
     store = serializers.CharField(
         help_text="コード", max_length=3, allow_null=True, required=False)
+
+    def validate_store(self, value):
+        if '111' not in value:
+            raise serializers.ValidationError("111以外はエラーです")
+        return value
