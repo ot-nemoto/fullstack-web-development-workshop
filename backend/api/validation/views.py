8d3c01aee8b39543d78d6d9d743f74931082eded
n-itou <itou@opentone.co.jp>
2022-10-22 17:47:59 +0900

    ネスト

--- backend/api/validation/views.py
+++ backend/api/validation/views.py
@@ -8,7 +8,7 @@ class ValidationView(views.APIView):
     def post(self, request):
         # http://127.0.0.1:8000/api/validation/
         # POST {"delivery_date":"hoge", "store":"hoge"}
-        # POST {"delivery_date":"2022-10-19", "store":"123"}
+        # POST {"delivery_date":"2022-10-10", "store":"111","price_product":[{"store":"222"}]}
         serializer = ValidationSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         return Response(serializer.data, status.HTTP_200_OK)
