fd1637b4d55716a5c14c4ac9d0f842ec032f1fa5
n-itou <itou@opentone.co.jp>
2023-01-05 17:58:41 +0900

    第四章

--- frontend/app/hello_frontend_axios/page.tsx
+++ frontend/app/hello_frontend_axios/page.tsx
@@ -2,21 +2,22 @@
 【執筆メモStart】
 フロントエンド内でのAPI疎通が行えます。
 
-http://localhost:3000/hello_frontend_fetch で表示します。
+http://localhost:3000/hello_frontend_axios で表示します。
 
 https://nextjs.org/docs/basic-features/data-fetching/client-side
 【執筆メモEnd】
 */
 'use client'
 
-import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 export default function Page() {
   const [data, setData] = useState({ name: '' })
 
   useEffect(() => {
-    fetch('/api/hello')
-      .then((res) => res.json())
+    axios.get('/api/hello')
+      .then((res) => res.data)
       .then((data) => { setData(data) })
   }, [])
 
