044e88ff98bc2ada3527aa4dcf5f5a2fcafbcb78
n-itou <itou@opentone.co.jp>
2023-01-18 16:16:00 +0900

    第四章

---
+++ frontend/app/hello_frontend_state_ng_loop/page.tsx
@@ -0,0 +1,27 @@
+/*
+【執筆メモStart】
+フロントエンドでステートを使用します。
+
+http://localhost:3000/hello_frontend_state_ng_change で表示します。
+
+https://reactjs.org/docs/faq-state.html
+setState() schedules an update to a component’s state object. When state changes, the component responds by re-rendering.
+のとおり、ステートが変わると再レンダリング（Page()を再呼出）します。
+ステートが変わる→再レンダリング→ステートが変わる…を無限に繰り返して
+「Error: Too many re-renders. React limits the number of renders to prevent an infinite loop.」エラーになります。
+
+https://nextjs.org/docs/basic-features/data-fetching/client-side
+【執筆メモEnd】
+*/
+'use client'
+
+import { useState } from 'react'
+
+export default function Page() {
+  const [data, setData] = useState({ name: '初期値' })
+
+  const change = { name: '変更' }
+  setData(change)
+
+  return <div>hello {data.name}!</div>
+}
