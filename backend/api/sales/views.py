eb415a5233ffdfe03a588a4a19afb1e1cc38445c
n-itou <itou@opentone.co.jp>
2023-02-01 14:41:59 +0900

    第9章途中まで

--- backend/api/sales/views.py
+++ backend/api/sales/views.py
@@ -5,31 +5,41 @@ http://127.0.0.1:8000/api/sales/
 Salesモデルのid=1のレコードが返却されます。
 
 https://www.django-rest-framework.org/api-guide/parsers/#fileuploadparser
+https://docs.python.org/ja/3/library/functions.html
+https://pandas.pydata.org/
 【執筆メモEnd】
 """
+import pandas
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from .models import Sales, SalesFile
 from .serializers import FileSerializer
-from .models import Sales
-import csv
-from django.http.multipartparser import MultiPartParser
-import io
+
 
 class SalesView(APIView):
 
     def post(self, request, format=None):
         serializer = FileSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        
-        print(serializer.validated_data['file'].size)
 
-        # Read csv file InMemoryUploadedFile
-        file = serializer.validated_data['file'].read().decode('utf-8-sig')
+        filename = serializer.validated_data['file'].name
 
-        io_string = io.StringIO(file)
-        for line in csv.reader(io_string):
-            print(line)
+        with open(filename, 'wb') as f:
+            f.write(serializer.validated_data['file'].read())
 
+        sales_file = SalesFile(sales_date='2022-01-01', file_name=filename)
+        sales_file.save()
+
+        df = pandas.read_csv(filename)
+        for _, row in df.iterrows():
+            date = row['date']
+            price = row['price']
+            print(date)
+            print(price)
+
+            sales = Sales(price=price, import_file=sales_file)
+            sales.save()
 
         return Response(status=201)
 
