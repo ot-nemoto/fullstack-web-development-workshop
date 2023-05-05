bcd659b5f0914c525e8257895163c0dc4b5367ab
keiji.ueno <keiji.ueno@opentone.co.jp>
2023-05-05 16:30:03 +0900

    処理がformを使ったMPAで行われるように修正

--- frontend/app/inventory/page.tsx
+++ frontend/app/inventory/page.tsx
@@ -8,150 +8,188 @@ import { useState, useEffect } from 'react';
 import productsData from "./sample/dummy_products.json";
 import Link from "next/link";
 
+type FormData = {
+  id: number | null
+  name: string
+  price: number
+  description: string
+}
+
 export default function Page() {
   // 読込データを保持
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<Array<FormData>>([]);
 
   useEffect(() => {
     setData(productsData);
   }, [])
 
   // 登録データを保持
-  const [name, setName] = useState("")
-  const [price, setPrice] = useState(0)
-  const [description, setDescription] = useState("")
+  const [id, setId] = useState<number | null>(0)
+  const [name, setName] = useState<string>("")
+  const [price, setPrice] = useState<string>("0")
+  const [description, setDescription] = useState<string>("")
+
+  // submit時のactionを分岐させる
+  const [action, setAction] = useState<string>("")
+
+  const handleSubmit = (event: any): void => {
+    const data: FormData = {
+      id: id,
+      name: name,
+      price: Number(price),
+      description: description
+    };
+
+    if (action === "add") {
+      handleAdd(data)
+    } else if (action === "update") {
+      if (data.id === null) {
+        return
+      }
+      handleEdit(data)
+    } else if (action === "delete") {
+      if (data.id === null) {
+        return
+      }
+      handleDelete(data.id)
+    }
+  };
 
   // 登録処理
-  const [shownNewRow, setShownNewRow] = useState(false);
   const handleShowNewRow = () => {
-    setEditingRow(0)
-    setShownNewRow(true)
+    setId(null)
+    setName("")
+    setPrice("0")
+    setDescription("")
   };
   const handleAddCancel = () => {
-    setShownNewRow(false)
+    setId(0)
   };
-  const handleAdd = () => {
-    // TODO: バックエンドを使用した登録処理を呼ぶ
-    const ob = {
-      name: name,
-      price: price,
-      description: description
-    }
-    console.dir(ob)
-    setShownNewRow(false)
+  const handleAdd = (data: FormData) => {
+    setId(0)
   };
 
   // 更新・削除処理
-  const [editingRow, setEditingRow] = useState(0);
-  const handleEditRow = (id) => {
-    setShownNewRow(false)
-    setEditingRow(id)
+  const handleEditRow = (id: number | null) => {
+    const selectedProduct: FormData = data.find(v => v.id === id)
+    setId(selectedProduct.id)
+    setName(selectedProduct.name)
+    setPrice(selectedProduct.price.toString())
+    setDescription(selectedProduct.description)
   };
-  const handleEditCancel = (id) => {
-    setEditingRow(0)
+  const handleEditCancel = () => {
+    setId(0)
   };
-  const handleEdit = (id) => {
-    const ob = {
-      id: id,
-      name: name,
-      price: price,
-      description: description
-    }
-    console.dir(ob)
-    setEditingRow(0)
+  const handleEdit = (data: FormData) => {
+    setId(0)
   };
-  const handleDelete = (id) => {
-    const ob = {
-      id: id
-    }
-    console.dir(ob)
-    setEditingRow(0)
+  const handleDelete = (id: number) => {
+    setId(id)
  };
 
   return (
     <div>
       <h2>商品一覧</h2>
       <button type="button" onClick={handleShowNewRow}>商品を追加する</button>
-      <table>
-        <thead>
-          <tr>
-            <th>商品ID</th>
-            <th>商品名</th>
-            <th>単価</th>
-            <th>説明</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {shownNewRow ? (
+      <form onSubmit={handleSubmit}>
+        <table>
+          <thead>
             <tr>
-              <td></td>
-              <td><input 
-                type="text" 
-                id="name"
-                value={name}
-                onChange={(event) => setName(event.target.value)}
-              /></td>
-              <td><input 
-                type="number" 
-                id="price"
-                value={price}
-                onChange={(event) => setPrice(event.target.value)}
-              /></td>
-              <td><input 
-                type="text" 
-                id="description"
-                value={description}
-                onChange={(event) => setDescription(event.target.value)}
-              /></td>
-              <td></td>
-              <td><button onClick={() => handleAddCancel()}>キャンセル</button><button onClick={() => handleAdd()}>登録する</button></td>
+              <th>商品ID</th>
+              <th>商品名</th>
+              <th>単価</th>
+              <th>説明</th>
+              <th></th>
+              <th></th>
             </tr>
-          ) : ""}
-          {data.map((data: any) => (
-            editingRow === data.id ? (
-              <tr key={data.id}>
-                <td>{data.id}</td>
-                <td><input 
-                  type="text" 
-                  id="name"
-                  // FIXME: onChangeイベントが実行されない限り、valueに設定された値にdata.nameが入らず空になる
-                  // defaultValueとvalueは同時に指定できない
-                  // defaultValue={data.name} 
-                  value={name}
-                  onChange={(event) => setName(event.target.value)}
-                /></td>
+          </thead>
+          <tbody>
+            {id === null ? (
+              <tr>
+                <td></td>
+                <td>
+                  <input 
+                    type="text" 
+                    id="name"
+                    value={name}
+                    onChange={(event) => setName(event.target.value)}
+                  />
+                </td>
                 <td><input 
                   type="number" 
                   id="price"
-                  // defaultValue={data.price} 
                   value={price}
                   onChange={(event) => setPrice(event.target.value)}
                 /></td>
                 <td><input 
                   type="text" 
                   id="description"
-                  // defaultValue={data.description} 
                   value={description}
                   onChange={(event) => setDescription(event.target.value)}
                 /></td>
                 <td></td>
-                <td><button onClick={() => handleEditCancel(data.id)}>キャンセル</button><button onClick={() => handleEdit(data.id)}>更新する</button><button onClick={() => handleDelete(data.id)}>削除する</button></td>
+                <td>
+                  <button type="button" onClick={() => handleAddCancel()}>キャンセル</button>
+                  <button
+                    type="submit"
+                    onClick={() => setAction("add")}
+                  >登録する</button>
+                </td>
               </tr>
-            ) : (
-              <tr key={data.id}>
-                <td>{data.id}</td>
-                <td>{data.name}</td>
-                <td>{data.price}</td>
-                <td>{data.description}</td>
-                <td><Link href={`/inventory/${data.id}`}>在庫処理</Link></td>
-                <td><button onClick={() => handleEditRow(data.id)}>更新・削除</button></td>
-              </tr>
-            )
-          ))}
-        </tbody>
-      </table>
+            ) : ""}
+            {data.map((data: any) => (
+              id === data.id ? (
+                <tr key={data.id}>
+                  <td>{data.id}</td>
+                  <td><input 
+                    type="text" 
+                    id="name"
+                    // FIXME: onChangeイベントが実行されない限り、valueに設定された値にdata.nameが入らず空になる
+                    // defaultValueとvalueは同時に指定できない
+                    value={name}
+                    onChange={(event) => setName(event.target.value)}
+                  /></td>
+                  <td><input 
+                    type="number" 
+                    id="price"
+                    value={price}
+                    onChange={(event) => setPrice(event.target.value)}
+                  /></td>
+                  <td><input 
+                    type="text" 
+                    id="description"
+                    value={description}
+                    onChange={(event) => setDescription(event.target.value)}
+                  /></td>
+                  <td></td>
+                  <td>
+                    <button type="button" onClick={() => handleEditCancel()}>キャンセル</button>
+                    <button
+                      type="submit"
+                      onClick={() => setAction("update")}
+                    >更新する</button>
+                    <button
+                      type="submit"
+                      onClick={() => setAction("delete")}
+                    >削除する</button>
+                  </td>
+                </tr>
+              ) : (
+                <tr key={data.id}>
+                  <td>{data.id}</td>
+                  <td>{data.name}</td>
+                  <td>{data.price}</td>
+                  <td>{data.description}</td>
+                  <td><Link href={`/inventory/${data.id}`}>在庫処理</Link></td>
+                  <td>
+                    <button type="button" onClick={() => handleEditRow(data.id)}>更新・削除</button>
+                  </td>
+                </tr>
+              )
+            ))}
+          </tbody>
+        </table>
+      </form>
     </div>
   )
 }
