382211cc297c0b96e425f4b002a3f5eb5d09a3a6
keiji.ueno <keiji.ueno@opentone.co.jp>
2023-05-07 09:44:44 +0900

    inventoryアプリケーションと疎通するミニマムなコードを追加

---
+++ backend/api/inventory/urls.py
@@ -0,0 +1,12 @@
+"""
+【執筆メモStart】
+URLとviews.pyの紐づけを行っています。
+https://www.django-rest-framework.org/api-guide/routers/
+【執筆メモEnd】
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.ProductView.as_view())
+]
--- backend/api/inventory/views.py
+++ backend/api/inventory/views.py
@@ -2,6 +2,10 @@
 【執筆メモStart】
 【執筆メモEnd】
 """
-from django.shortcuts import render
+from rest_framework import status, views
+from rest_framework.response import Response
 
-# Create your views here.
+# Create your views here.class ProductView(views.APIView):
+    def get(self, request):
+        return Response({"message": "product"})
+        
