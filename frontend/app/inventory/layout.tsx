ee838c4f5c5da4de57197efb96fa4b34aff46bd6
keiji.ueno <keiji.ueno@opentone.co.jp>
2023-06-02 11:48:20 +0900

    メニューボタン押下によってサイドバーが開閉するように修正

--- frontend/app/inventory/layout.tsx
+++ frontend/app/inventory/layout.tsx
@@ -8,31 +8,40 @@ https://mui.com/material-ui/react-drawer/
 */
 "use client";
 
+import { useState } from "react";
 import { deleteCookie, getCookie } from "../../utils/cookie";
 import {
   AppBar,
   Box,
   Button,
   Divider,
-  Toolbar,
-  Typography,
   Drawer,
+  IconButton,
+  Link,
   List,
   ListItem,
   ListItemButton,
   ListItemText,
-  Link,
+  Toolbar,
+  Typography,
 } from "@mui/material";
-import { Logout as LogoutIcon } from "@mui/icons-material";
+import { Logout as LogoutIcon, Menu as MenuIcon } from "@mui/icons-material";
 import { useRouter } from "next/navigation";
-
-const drawerWidth = 240;
 
 export default function InventoryLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  /** サイドバーの開閉を管理する */
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (open: boolean) => {
+    console.dir(open);
+    setOpen(open);
+  };
+
+  /** 各種画面への遷移を管理する */
   const router = useRouter();
 
   const isLoggedIn = () => {
@@ -50,13 +59,35 @@ export default function InventoryLayout({
     router.replace("/login");
   };
 
+  /** 開閉対象となるサイドバー本体 */
+  const list = () => (
+    <Box sx={{ width: 240 }}>
+      <Toolbar />
+      <Divider />
+      <List>
+        <ListItem component="a" href="/inventory/products" disablePadding>
+          <ListItemButton>
+            <ListItemText primary="商品一覧" />
+          </ListItemButton>
+        </ListItem>
+        <Divider />
+        <ListItem component="a" href="/inventory/import_sales" disablePadding>
+          <ListItemButton>
+            <ListItemText primary="売上一括登録" />
+          </ListItemButton>
+        </ListItem>
+        <Divider />
+      </List>
+    </Box>
+  );
+
   return (
     <Box sx={{ display: "flex" }}>
-      <AppBar
-        position="fixed"
-        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
-      >
+      <AppBar position="fixed">
         <Toolbar>
+          <IconButton onClick={() => toggleDrawer(true)}>
+            <MenuIcon />
+          </IconButton>
           <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
             在庫管理システム
           </Typography>
@@ -69,39 +100,14 @@ export default function InventoryLayout({
           </Button>
         </Toolbar>
       </AppBar>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          "& .MuiDrawer-paper": {
-            width: drawerWidth,
-            boxSizing: "border-box",
-          },
-        }}
-        variant="permanent"
-        anchor="left"
-      >
-        <Toolbar />
-        <Divider />
-        <List>
-          <ListItem component="a" href="/inventory/products" disablePadding>
-            <ListItemButton>
-              <ListItemText primary="商品一覧" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem component="a" href="/inventory/import_sales" disablePadding>
-            <ListItemButton>
-              <ListItemText primary="売上一括登録" />
-            </ListItemButton>
-          </ListItem>
-        </List>
+      <Drawer open={open} onClose={() => toggleDrawer(false)} anchor="left">
+        {list()}
       </Drawer>
       <Box
         component="main"
         sx={{
           flexGrow: 1,
           p: 3,
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
           // TODO: AppBarと被るため下にずらしている。本来は動的に取得すべき
           marginTop: "64px",
           background: "white",
