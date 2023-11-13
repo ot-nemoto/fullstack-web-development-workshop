e12184cdc4e5fbd7dd2f0cd33d9b732446544d48
n-itou <itou@opentone.co.jp>
2023-11-13 20:11:37 +0900

    図6-5-6まで

---
+++ chapter5_done_itou/backend/api/inventory/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('products/', views.ProductView.as_view()),
+    path('products/model/', views.ProductModelViewSet.as_view({ 'get': 'list'})),
+]
