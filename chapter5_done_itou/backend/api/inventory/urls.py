4ef0d8148875a452d623837202a158a996b80584
n-itou <itou@opentone.co.jp>
2023-11-13 22:12:43 +0900

    図6-8-2まで

--- chapter5_done_itou/backend/api/inventory/urls.py
+++ chapter5_done_itou/backend/api/inventory/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('products/', views.ProductView.as_view()),
     path('products/<int:id>/', views.ProductView.as_view()),
@@ -7,4 +12,7 @@ urlpatterns = [
     path('inventories/<int:id>/', views.InventoryView.as_view() ),
     path('purchases/', views.PurchaseView.as_view()),
     path('sales/', views.SalesView.as_view()),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('products/', views.ProductView.as_view()),
 ]
--- chapter5_done_itou/backend/config/settings/base.py
+++ chapter5_done_itou/backend/config/settings/base.py
@@ -140,3 +140,10 @@ LOGGING = {
         }
     }
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated']
+}
