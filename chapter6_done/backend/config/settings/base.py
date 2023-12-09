ffdd74e33e782e4c55f52025fb26b33985ac9557
n-itou <itou@opentone.co.jp>
2023-12-09 18:58:48 +0900

    no message

--- chapter6_done/backend/config/settings/base.py
+++ chapter6_done/backend/config/settings/base.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-import datetime
+from timedelta import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
