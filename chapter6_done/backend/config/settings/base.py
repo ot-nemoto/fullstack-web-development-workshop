17b567790ee015f0f9032feda54023b140d54ae8
n-itou <itou@opentone.co.jp>
2023-12-09 18:32:19 +0900

    6章誤り？

--- chapter6_done/backend/config/settings/base.py
+++ chapter6_done/backend/config/settings/base.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from timedelta import datetime
+from datetime import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
