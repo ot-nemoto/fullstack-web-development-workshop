d2fea228fbeeb94f8b0ac91d6d19ea52b092a70d
n-itou <itou@opentone.co.jp>
2023-12-09 19:04:42 +0900

    コード7-1-3まで

--- chapter7_done/backend/api/inventory/views.py
+++ chapter7_done/backend/api/inventory/views.py
@@ -164,3 +164,12 @@ class LogoutView(APIView):
         response.delete_cookie('access')
         response.delete_cookie('refresh')
         return response
+
+class SalesAsyncView(APIView):
+    pass
+
+class SalesSyncView(APIView):
+    pass
+
+class SalesList(APIView):
+    pass
