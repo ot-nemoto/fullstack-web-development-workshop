3d2514ae73922f49f82a4bb0e32313a2fb6bfb37
n-itou <itou@opentone.co.jp>
2023-12-09 18:41:59 +0900

    6章→7章コピー

--- chapter7_done/backend/config/settings/base.py
+++ chapter7_done/backend/config/settings/base.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,7 +41,6 @@ INSTALLED_APPS = [
     "rest_framework",
     "api.hello_db",
     "api.inventory",
-    "batch",
 ]
 
 MIDDLEWARE = [
@@ -141,3 +141,20 @@ LOGGING = {
         }
     }
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'api.inventory.authentication.CustomJWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated']
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=15),
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=30),
+    'UPDATE_LAST_LOGIN': True,
+}
+
+# クッキーの有効期限に使用する
+COOKIE_TIME = 60 * 60 * 12
--- chapter7_done/frontend/app/inventory/products/[id]/page.tsx
+++ chapter7_done/frontend/app/inventory/products/[id]/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 
-"use client";
-
+import axios from "../../../../plugins/axios";
 import {
+    Alert,
+    AlertColor,
     Box,
     Button,
     Paper,
+    Snackbar,
     Table,
     TableBody,
     TableCell,
@@ -17,8 +19,6 @@ import {
 } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { useState, useEffect } from 'react';
-import productsData from "../sample/dummy_products.json";
-import inventoriesData from "../sample/dummy_inventories.json";
 
 type ProductData = {
     id: number;
@@ -34,7 +34,7 @@ type FormData = {
 
 type InventoryData = {
     id: number;
-    type: string;
+    type: number;
     date: string;
     unit: number;
     quantity: number;
@@ -55,19 +55,49 @@ export default function PagePage({ params }: {
     // 読込データを保持
     const [product, setProduct] = useState<ProductData>({ id: 0, name: "", price: 0,  description: ""});
     const [data, setData] = useState<Array<InventoryData>>([]);
+    const [open, setOpen] = useState(false);
+    const [severity, setSeverity] = useState<AlertColor>('success');
+    const [message, setMessage] = useState('');
+    const result = (severity: AlertColor, message: string) => {
+        setOpen(true);
+        setSeverity(severity);
+        setMessage(message);
+    };
     // submit時のactionを分岐させる
     const [action, setAction] = useState<string>("");
 
+    const handleClose = (event: any, reason: any) => {
+        setOpen(false);
+    };
+
     useEffect(() => {
-        const selectedProduct: ProductData = productsData.find(v => v.id == params.id) ?? {
-            id: 0,
-            name: "",
-            price: 0,
-            description: "",
-          };
-        setProduct(selectedProduct);
-        setData(inventoriesData);
-    }, [])
+        axios.get(`/api/inventory/products/${params.id}`)
+            .then((response) => {
+                setProduct(response.data);
+        });
+        axios.get(`/api/inventory/inventories/${params.id}`)
+            .then((response) => {
+                const inventoryData: InventoryData[] = [];
+                let key: number = 1;
+                let inventory: number = 0;
+
+                response.data.forEach((e: InventoryData) => {
+                    // 売るときは在庫数から引く
+                    inventory += e.type === 1 ? e.quantity : e.quantity * -1;
+                    const newElement = {
+                        id: key++,
+                        type: e.type,
+                        date: e.date,
+                        unit: e.unit,
+                        quantity: e.quantity,
+                        price: e.unit * e.quantity,
+                        inventory: inventory,
+                    };
+                    inventoryData.unshift(newElement);
+                });
+                setData(inventoryData);
+        });
+    }, [open])
 
     const onSubmit = (event: any): void => {
         const data: FormData = {
@@ -88,15 +118,32 @@ export default function PagePage({ params }: {
 
     // 仕入れ・卸し処理
     const handlePurchase = (data: FormData) => {
-        alert("作成完了");
+        const purchase = {
+            quantity: data.quantity,
+            purchase_date: new Date(),
+            product: data.id,
+        };
+        axios.post("/api/inventory/purchases", purchase).then((response) => {
+            result('success', '商品を仕入れました')
+        });
     };
 
     const handleSell = (data: FormData) => {
-        alert("作成完了");
+        const sale = {
+            quantity: data.quantity,
+            sales_date: new Date(),
+            product: data.id,
+        };
+        axios.post("/api/inventory/sales", sale).then((response) => {
+            result('success', '商品を卸しました')
+        });
     };
 
     return (
         <>
+            <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+                <Alert severity={severity}>{message}</Alert>
+            </Snackbar>
             <Typography variant="h5">商品在庫管理</Typography>
             <Typography variant="h6">在庫処理</Typography>
             <Box component="form" onSubmit={handleSubmit(onSubmit)}>
@@ -162,8 +209,8 @@ export default function PagePage({ params }: {
                     <TableBody>
                         {data.map((data: InventoryData) => (
                             <TableRow key={data.id}>
-                                <TableCell>{data.type}</TableCell>
-                                <TableCell>{data.date}</TableCell>
+                                <TableCell>{data.type === 1 ? "仕入れ" : "卸し"}</TableCell>
+                                <TableCell>{new Date(data.date).toLocaleDateString()}</TableCell>
                                 <TableCell>{data.unit}</TableCell>
                                 <TableCell>{data.quantity}</TableCell>
                                 <TableCell>{data.price}</TableCell>
