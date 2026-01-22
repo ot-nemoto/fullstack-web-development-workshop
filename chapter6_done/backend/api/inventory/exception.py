ee65207624684f13ec8b1e6bd3537f07843a479f
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2026-01-22 11:13:15 +0900

    Remove sales data and command documentation; add chapters on web system development, VSCode with Docker, and React+Django environment setup.

--- chapter6_done/backend/api/inventory/exception.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import status
-from rest_framework.exceptions import ValidationError
-
-class BusinessException(ValidationError):
-    status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
